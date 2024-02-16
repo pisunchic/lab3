7ecef99bfadf25fdd1d2a431ab32dcc1fb11ef97
pisunchic <120672338+pisunchic@users.noreply.github.com>
2024-02-16 18:14:28 +0300

    add ScaffoldScreen

---
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ScaffoldScreen.kt
@@ -0,0 +1,29 @@
+package com.raywenderlich.android.lab1.screens
+
+import androidx.compose.material.BackdropScaffoldState
+import androidx.compose.material.ScaffoldState
+import androidx.compose.runtime.Composable
+import com.raywenderlich.android.lab1.router.BackButtonHandler
+import com.raywenderlich.android.lab1.router.FundamentalsRouter
+import com.raywenderlich.android.lab1.router.Screen
+
+@Composable
+fun ScaffoldScreen(){
+    MyScaffold()
+    BackButtonHandler {
+        FundamentalsRouter.navigateTo(Screen.Navigation)
+    }
+}
+
+@Composable
+fun MyScaffold() {
+
+}
+@Composable
+fun MyTopAppBar(scaffoldState: ScaffoldState) {
+
+}
+@Composable
+fun MyBottonAppBar(){
+
+}
