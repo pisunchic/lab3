b063df5de278d5f53fd53a5b2f83a9d5c01bc5e3
pisunchic <120672338+pisunchic@users.noreply.github.com>
2024-02-17 20:16:07 +0300

    add VerticalScreen

--- app/src/main/java/com/raywenderlich/android/lab1/screens/NavigationScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/NavigationScreen.kt
@@ -39,6 +39,8 @@ fun NavigationScreen() {
             NavigationButton(stringResource(id = R.string.button_box), Screen.Box)
             NavigationButton(stringResource(id = R.string.button_surface), Screen.Surface)
             NavigationButton(stringResource(id = R.string.button_scaffold), Screen.Scaffold)
+            NavigationButton(stringResource(id = R.string.button_scrolling), Screen.Scrolling)
+
 
 
 
